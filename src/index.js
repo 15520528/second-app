a38075290d248143f425b599b21dd0414ea51b25
nam <namvh3@vng.com.vn>
2019-01-15 21:12:01 +0700

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Home from './components/Home';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Topic() {
+    return (
+        <div>
+            <h2>Components</h2>
+        </div>
+    );
+}
+
+const footer  = ({ match })=>(
+    <div>
+        <h2>footer</h2>
+        <ul>
+            <li>
+                <Link to={`${match.url}/components`}>Components</Link>
+            </li>
+
+        </ul>
+
+        <Route path={`${match.path}/:id`} component={Topic} />
+        <Route
+            exact
+            path={match.path}
+            render={() =>  <h3>Please select a topic. {console.log(match.path)}</h3> }
+        />
+    </div>
+);
+
+ReactDOM.render(
+    (<Router  >
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/footer">footer</Link>
+                </li>
+            </ul>
+            <Route exact path="/" component={App} />
+            <Route path = "/about" component = {About} />
+            <Route path = "/footer" component = {footer} />
+        </div>
+    </Router>),
+    document.getElementById('root'));
+
